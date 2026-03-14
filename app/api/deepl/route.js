78c41b929db212a9a9b22324c9ae1fa3139be797
Anggun Pribadi <anxggxun@gmail.com>
2026-03-14 07:08:48 +0700

    Add split_sentences option to translation request

--- app/api/deepl/route.js
+++ app/api/deepl/route.js
@@ -5,7 +5,7 @@ const translator = new deepl.Translator(authKey);
 
 export async function POST(request) {
   const tw = await request.json()
-  const translated = await translator.translateText(tw.text, null, 'en-US');
+  const translated = await translator.translateText(tw.text, null, 'en-US', {split_sentences: 0});
   console.log(tw)
   return Response.json( { text: translated.text } )
 }
